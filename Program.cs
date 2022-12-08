1a8eba4d750d9caf55bfccaf0569d38164607440
Pratik Chakraborty <pratik.c@flatworldlocal.com>
2022-12-08 13:35:40 +0530

    update

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Xml.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Data;
+using Microsoft.CognitiveServices.Speech.Translation;
+
+class Program
+{
+    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    static string speechKey = "af8c8f8514a84e70888a3b07fbf9e353";
+    static string key = "68bc6f57061d40d6b03bfdaebf28de08";
+    static string end = "https://api.cognitive.microsofttranslator.com/";
+    static string speechRegion = "centralindia";
+
+    static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
+    {
+        switch (speechRecognitionResult.Reason)
+        {
+            case ResultReason.RecognizedSpeech:
+                Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                break;
+            case ResultReason.NoMatch:
+                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                break;
+            case ResultReason.Canceled:
+                var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
+    }
+
+    async static Task Main(string[] args)
+    {
+       string text= await speechToText();
+        await translate(text);
+    }
+
+    private static async Task translate(string text)
+    {
+        string route = "/translate?api-version=3.0&from=en&to=fr";
+        string textToTranslate = "hello";
+        object[] body = new object[] { new { Text = textToTranslate } };
+        var requestBody = JsonConvert.SerializeObject(body);
+
+        using (var client = new HttpClient())
+        using (var request = new HttpRequestMessage())
+        {
+            // Build the request.
+            request.Method = HttpMethod.Post;
+            request.RequestUri = new Uri(end + route);
+            request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+            request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+            // location required if you're using a multi-service or regional (not global) resource.
+            request.Headers.Add("Ocp-Apim-Subscription-Region", speechRegion);
+
+            // Send the request and get response.
+            HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+            // Read response as a string.
+            string result = await response.Content.ReadAsStringAsync();
+            //var results= result.Split("");
+            Console.WriteLine(result);
+        }
+    }
+
+    private async static Task<string> speechToText()
+    {
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+        speechConfig.SpeechRecognitionLanguage = "en-US";
+
+        var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        Console.WriteLine("Speak into your microphone.");
+        var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+        OutputSpeechRecognitionResult(speechRecognitionResult);
+        Console.WriteLine($"You just said {speechRecognitionResult.Text}");
+        return speechRecognitionResult.Text;
+    }
+
+}
